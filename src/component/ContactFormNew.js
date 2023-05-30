8e6f0ff27375ddd531d44484e138078c8b95e996
sagarbharvadiya <sagarbharvadiya@gmail.com>
2023-05-30 17:58:58 +0530

    contact form

--- src/component/ContactFormNew.js
+++ src/component/ContactFormNew.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, push } from 'firebase/database';
 
-
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  databaseURL: 'YOUR_DATABASE_URL',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: '1:926290748516:web:b342288b9b51ca028d6b71',
-  measurementId: 'YOUR_MEASUREMENT_ID',
+  apiKey: process.env.GATSBY_YOUR_API_KEY,
+  authDomain: process.env.GATSBY_YOUR_AUTH_DOMAIN,
+  databaseURL: process.env.GATSBY_YOUR_DATABASE_URL,
+  projectId: process.env.GATSBY_YOUR_PROJECT_ID,
+  storageBucket: process.env.GATSBY_YOUR_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_YOUR_MESSAGING_SENDER_ID,
+  appId: process.env.GATSBY_YOUR_APP_ID,
+  measurementId: process.env.GATSBY_YOUR_MEASUREMENT_ID,
 };
 
-initializeApp(firebaseConfig);
-const database = getDatabase();
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
 const ContactFormNew = () => {
   const [details, setDetails] = useState({
@@ -25,13 +23,31 @@ const ContactFormNew = () => {
     sub: '',
     mes: '',
   });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+
+  const validateForm = () => {
+    const { fName, email, sub, mes } = details;
+
+    if (fName.trim() === '' || email.trim() === '' || sub.trim() === '' || mes.trim() === '') {
+      return false;
+    }
+
+    return true;
+  };
 
   const postData = async (e) => {
     e.preventDefault();
+
+    if (!validateForm()) {
+      return;
+    }
+
     const { fName, email, sub, mes } = details;
 
     try {
       const formRef = ref(database, 'sagarform');
+      setIsSubmitting(true);
       await push(formRef, {
         fName,
         email,
@@ -47,9 +63,12 @@ const ContactFormNew = () => {
         mes: '',
       });
 
-      console.log('Data posted successfully');
+      setSubmitted(true);
+      alert('Data posted successfully');
     } catch (error) {
       console.error('Error posting data:', error);
+    } finally {
+      setIsSubmitting(false);
     }
   };
 
@@ -67,6 +86,7 @@ const ContactFormNew = () => {
               value={details.fName}
               onChange={(e) => setDetails({ ...details, fName: e.target.value })}
               required
+              disabled={submitted}
             />
           </div>
           <div className="col-md-6 form-group mt-3 mt-md-0">
@@ -79,6 +99,7 @@ const ContactFormNew = () => {
               value={details.email}
               required
               onChange={(e) => setDetails({ ...details, email: e.target.value })}
+              disabled={submitted}
             />
           </div>
         </div>
@@ -92,6 +113,7 @@ const ContactFormNew = () => {
             value={details.sub}
             required
             onChange={(e) => setDetails({ ...details, sub: e.target.value })}
+            disabled={submitted}
           />
         </div>
         <div className="form-group mt-3">
@@ -103,12 +125,13 @@ const ContactFormNew = () => {
             value={details.mes}
             required
             onChange={(e) => setDetails({ ...details, mes: e.target.value })}
+            disabled={submitted}
           ></textarea>
         </div>
         <div className="my-3"></div>
         <div className="text-center">
-          <button type="submit" onClick={postData}>
-            submit
+          <button type="submit" onClick={postData} disabled={isSubmitting || submitted}>
+            {isSubmitting ? 'Submitting...' : submitted ? 'Submitted' : 'Submit'}
           </button>
         </div>
       </form>
--- src/component/WhatIDo.jsx
+++ src/component/WhatIDo.jsx
@@ -8,7 +8,7 @@ const WhatIDo = () => {
     const settings = {
         dots: false,
         infinite: true,
-        speed: 500,
+        speed: 1000,
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true, // Add this line to enable autoplay
