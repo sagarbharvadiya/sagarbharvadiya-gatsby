43098cf06e97be983b9febb894f6950074cdefa3
sagarbharvadiya <sagarbharvadiya@gmail.com>
2023-11-01 12:51:15 +0530

    build

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,8 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
-
-/**
- * @type {import('gatsby').GatsbyNode['createPages']}
- */
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
     path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
+    component: require.resolve("./src/pages/Home.jsx"),
     context: {},
     defer: true,
   })
