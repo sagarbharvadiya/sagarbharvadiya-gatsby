4babecadeeadba3a686610d60bd2c2c26020cef3
sagarbharvadiya <sagarbharvadiya@gmail.com>
2024-07-24 11:22:27 +0530

    meta add

--- src/component/Meta.js
+++ src/component/Meta.js
@@ -6,13 +6,9 @@ const Meta = () => {
     <Helmet>
       <title>Web Developer | Sagar Bharvadiya</title>
       <meta name="description" content="Web Developer & Frontend Developer specializing in HTML, CSS, JavaScript, Responsive Design, User Experience, and Web Development." />
-      <meta
-        name="keywords"
-        content="Frontend UI Developer, Web Design, HTML, CSS, JavaScript, Responsive Design, User Experience, Web Development, Portfolio, Freelance, Hire Me, Contact Me, About Me, Blog, Projects, Resume, Skills, Services, Testimonials, Clients"
-      />
+      <meta name="keywords" content="Frontend UI Developer, Web Design, HTML, CSS, JavaScript, Responsive Design, User Experience, Web Development, Portfolio, Freelance, Hire Me, Contact Me, About Me, Blog, Projects, Resume, Skills, Services, Testimonials, Clients, Sagar Bharvadiya, sagar, Bharvadiya, ahir, bharvadiya sagar, Bharvadiya Sagar, website design, website development, web developer, ui/ux design, web design services, web development company, hire web developer, freelance web developer, web design portfolio, web development portfolio, responsive web design, mobile-friendly website, website redesign, website builder, website creator, web design agency, web development agency, digital marketing, online marketing, search engine optimization, SEO, google search, website optimization, website performance, website security, website maintenance, website updates, web design trends, web development trends, latest web technologies, web design best practices, web development best practices, website accessibility, website usability, user experience design, user interface design, web design inspiration, web development inspiration, website ideas, website concepts, website strategy, website planning, website execution, website launch, website promotion, website marketing, website analytics, website tracking, website metrics, website performance metrics" />
       <meta name="google-adsense-account" content="ca-pub-9210386825063873" />
       <meta name="google-site-verification" content="F7l2yl8BqtoSadT_AZqPRFpDdRuWOL51wx3C4RQeFUU" />
-      <meta name="google-site-verification" content="google-site-verification=xyJcr3DOB4muqe32E7a0J4hewD96LAtGpl_awBf4iUM" />
     </Helmet>
   );
 };
